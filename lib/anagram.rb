755ba55bc0c8809fc524789a244ad8eb552fdc52
Vincent Servio <vincentservio@gmail.com>
2020-02-23 19:16:24 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram 
+attr_accessor :anagram, :word
+
+  def initialize(word)
+      @word = word 
+     
+      #binding.pry
+  end 
+
+    def match(array)
+  array.keep_if do |wrd| 
+  
+
+    word.split("").sort == wrd.split("").sort
+  end 
+  end
+end
+
+#["hello", "world", "zombies", "pants", "dipper"]
+#(%w(hello world zombies pants dipper)
